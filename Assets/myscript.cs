083e1aca23b529b5425cb7c2023cb4e1f6f15c57
Bill K <Bill K>
2016-07-09 16:55:53 -0500

    added script

---
+++ Assets/myscript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class myscript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		print ("blah blah");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
